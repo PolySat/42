9e43ba9dbe7ef14ae4e88da95449028f9fa9b934
John Bellardo <bellardo@calpoly.edu>
2018-04-28 14:53:09 -0700

    Update test.py for GUI testing

--- Python/test.py
+++ Python/test.py
@@ -1,10 +1,29 @@
 #!/usr/bin/python3.6
 import nasa42
+import time
 
-sim = nasa42.Simulation()
+simulation = nasa42.Simulation()
 eci = nasa42.WORLD_INERTIAL_FRAME
+now = 1474416005
 
-for t in range(1474416005, 1474419100, 100):
-   print('Step to ' + str(t))
-   sim.propagate(t)
-   print('Position: ' + str(sim.spacecraft[0].position(eci)))
+def stepCallback(simp):
+    global now
+    global eci
+    #print('Py callback ' + str(sim))
+    if now > 1474419100:
+        print('Ending sim at ' + str(now))
+        return 1
+
+    now = now + 100
+    simp.propagate(now)
+    print('Position: ' + str(simp.spacecraft[0].position(eci)))
+    return 0
+
+def ssCallback(sim):
+    return sim.SimStep()
+
+simulation.propagate(now)
+simulation.startGUI(stepCallback)
+#for t in range(1474416005, 1474419100, 100):
+#   simulation.propagate(t)
+#   print('Position: ' + str(simulation.spacecraft[0].position(eci)))
