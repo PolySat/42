3fcda2a0e605fea3dca430e053d72b40befc80da
Luc Bouchard <lbouchard010@gmail.com>
2018-04-27 22:48:06 -0700

    Move python binding code into src directory

--- Python/setup.py
+++ Python/setup.py
@@ -3,4 +3,4 @@ import numpy as np
 from distutils.core import setup, Extension
 setup(name='nasa42', version='1.0', include_dirs = [np.get_include()],   \
       ext_modules=[Extension('nasa42', libraries = ['nasa42'], \
-      sources = ['42py.c', 'spacecraft.c', 'simulation.c', 'utils.c'])])
+      sources = ['src/42py.c', 'src/spacecraft.c', 'src/simulation.c', 'src/utils.c'])])
