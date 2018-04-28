6b74432ac1f316e7fd42b78ea13cf4ec913052d7
John Bellardo <bellardo@calpoly.edu>
2018-04-28 09:51:35 -0700

    Use python3.6 by default

--- Python/test.py
+++ Python/test.py
@@ -1,9 +1,10 @@
-#!/usr/bin/python
+#!/usr/bin/python3.6
 import nasa42
 
 sim = nasa42.Simulation()
 eci = nasa42.WORLD_INERTIAL_FRAME
 
 for t in range(1474416005, 1474419100, 100):
+   print('Step to ' + str(t))
    sim.propagate(t)
    print('Position: ' + str(sim.spacecraft[0].position(eci)))
